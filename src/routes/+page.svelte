36e96af0fbc5f58415e99373cff2e01fe71df7b9
Phongsakorn Wisetthon <boy1556@hotmail.com>
2023-02-23 13:16:37 +0700

    ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+// @ts-nocheck
+
+    import { 
+        Styles,
+        Collapse,
+        Navbar,
+        NavbarToggler,
+        NavbarBrand,
+        Nav,
+        NavItem,
+        NavLink,
+        Dropdown,
+        DropdownToggle,
+        DropdownMenu,
+        DropdownItem,
+        Container
+    } from 'sveltestrap';
+
+    let isOpen = false;
+
+    /**
+	 * @param {{ detail: { isOpen: boolean; }; }} event
+	 */
+    function handleUpdate(event) {
+        isOpen = event.detail.isOpen;
+    }
+</script>
+
+<Styles />
+
+<Navbar color="light" light expand="md">
+    <Container sm class="d-flex justify-content-between">
+        <NavbarBrand href="/">sveltestrap</NavbarBrand>
+        <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+        <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+        <Nav class="ms-auto" navbar>
+            <NavItem>
+            <NavLink href="#components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+            <NavLink href="https://github.com/bestguy/sveltestrap">GitHub</NavLink>
+            </NavItem>
+            <Dropdown nav inNavbar>
+            <DropdownToggle nav caret>Options</DropdownToggle>
+            <DropdownMenu end>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+            </DropdownMenu>
+            </Dropdown>
+        </Nav>
+        </Collapse>
+    </Container>
+</Navbar>
