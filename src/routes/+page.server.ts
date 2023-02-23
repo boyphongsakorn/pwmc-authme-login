cd28ba4156acea82cbd1ed737b1990026bb22498
Phongsakorn Wisetthon <boy1556@hotmail.com>
2023-02-24 01:14:28 +0700

    ok

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -35,31 +35,18 @@ export const load: PageServerLoad = async (event) => {
 }
 
 //create from when redirect to this page
-export const handle: Actions = async (event) => {
-    console.log(event)
-    console.log(event.cookies.get('disco_access_token'))
-    let disco_name = null
-    if (event.cookies.get('disco_access_token')) {
-        const request = await fetch('https://discord.com/api/users/@me', {
-            headers: {
-                'Authorization': `Bearer ${event.cookies.get('disco_access_token')}`
+export const handle: Actions = {
+    async redirect({ request, resolve }) {
+        const response = await resolve(request);
+        console.log(response)
+        if (response.status === 307) {
+            return {
+                status: 307,
+                headers: {
+                    'Location': response.headers['Location']
+                }
             }
-        })
-        const response = await request.json()
-        //check status
-        if (response.message === '401: Unauthorized') {
-            //redirect to login
-        } else {
-            //get user
-            disco_name = response.username
         }
-    }
-    //pass to $app/stores
-    return {
-        props: {
-            disco_access_token: event.cookies.get('disco_access_token'),
-            disco_refresh_token: event.cookies.get('disco_refresh_token'),
-            disco_name: disco_name
-        }
+        return response;
     }
 }
