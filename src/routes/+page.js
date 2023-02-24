d7b4bcd9079bd252047a449799bef51541faad8c
Phongsakorn Wisetthon <boy1556@hotmail.com>
2023-02-24 08:54:23 +0700

    ok

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -4,6 +4,11 @@ export function load({ params }) {
       post: {
         title: `Title for ${params.slug} goes here`,
         content: `Content for ${params.slug} goes here`
-      }
+      },
+      props: {
+            disco_access_token: params.cookies.get('disco_access_token'),
+            disco_refresh_token: params.cookies.get('disco_refresh_token'),
+            disco_name: null
+        }
     };
   }
