8d3653953f132faa50cf43dfbf88597e3398369e
Phongsakorn Wisetthon <boy1556@hotmail.com>
2023-02-24 09:36:27 +0700

    ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -41,9 +41,9 @@
     console.log(data);
 
     //if url has code refresh page to /
-    if ($page.query.code) {
-        window.location.href = '/';
-    }
+    // if ($page.query.code) {
+    //     window.location.href = '/';
+    // }
 </script>
 
 <Styles />
