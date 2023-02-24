4d236d3829a55ece9589d63ba8847f0a38ba1cc9
Phongsakorn Wisetthon <boy1556@hotmail.com>
2023-02-24 10:09:48 +0700

    ok

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,89 @@
+<script>
+    import { 
+        Styles,
+        Collapse,
+        Navbar,
+        NavbarToggler,
+        NavbarBrand,
+        Nav,
+        NavItem,
+        NavLink,
+        Dropdown,
+        DropdownToggle,
+        DropdownMenu,
+        DropdownItem,
+        Container,
+        Modal,
+        ModalBody,
+        ModalFooter,
+        ModalHeader,
+        Button
+    } from 'sveltestrap';
+    import { page } from '$app/stores';
+    import { onMount } from 'svelte';
+    import { goto } from '$app/navigation';
+    let isOpen = false;
+
+    /**
+	 * @param {{ detail: { isOpen: boolean; }; }} event
+	 */
+    function handleUpdate(event) {
+        isOpen = event.detail.isOpen;
+    }
+
+    let open = false;
+    const toggle = () => (open = !open);
+
+    console.log($page);
+
+    onMount(async () => {
+        // if ($page.data.props.disco_access_token === undefined || $page.data.props.disco_access_token === 'undefined' || $page.data.props.disco_access_token === null) {
+        //     goto('/', { invalidateAll: true });
+        // }
+    });
+</script>
+
+<Styles />
+
+<Navbar color="dark" dark expand="md">
+    <Container sm class="d-flex justify-content-between">
+        <NavbarBrand href="/">BP Minecraft Server</NavbarBrand>
+        <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+        <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+        <Nav class="ms-auto" navbar>
+            <!--NavItem>
+            <NavLink href="#components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+            <NavLink href="https://github.com/bestguy/sveltestrap">GitHub</NavLink>
+            </NavItem>
+            <Dropdown nav inNavbar>
+            <DropdownToggle nav caret>Options</DropdownToggle>
+            <DropdownMenu end>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+            </DropdownMenu>
+            </Dropdown-->
+            {#if $page.data.props.disco_access_token !== undefined && $page.data.props.disco_access_token !== 'undefined' && $page.data.props.disco_access_token !== null}
+                <NavItem>
+                    <NavLink href="https://bpminecraft.com/profile">คุณ {$page.data.props.disco_name}</NavLink>
+                </NavItem>
+                <NavItem>
+                    <Button style="background-color: #5865F2;" href="https://bpminecraft.com/api/discordlogout" rel="external">ออกจากระบบ</Button>
+                </NavItem>
+            {/if}
+        </Nav>
+        </Collapse>
+    </Container>
+</Navbar>
+
+<Container sm>
+    <!-- {#if $page.data.props.disco_access_token !== undefined && $page.data.props.disco_access_token !== 'undefined' && $page.data.props.disco_access_token !== null}
+    
+    {/if} -->
+    <h1>Profile</h1>
+    <p>คุณ {$page.data.props.disco_name}</p>
+    
+</Container>
