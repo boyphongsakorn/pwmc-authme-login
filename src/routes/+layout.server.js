3d864ae04794b6f28effdc1bf97a20be69f7b95f
Phongsakorn Wisetthon <boy1556@hotmail.com>
2023-02-24 09:07:05 +0700

    ok

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,5 +1,8 @@
 export const load = async ({ request, locals, cookies }) => {
     console.log(cookies);
+    console.log(request.headers.cookie);
+    console.log(request.headers);
+    console.log(cookies.get('disco_access_token'));
     return {
         session: locals.session
     };
