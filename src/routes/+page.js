90fb47870337c619a6210943756c32dc0e859953
Phongsakorn Wisetthon <boy1556@hotmail.com>
2023-02-24 08:52:21 +0700

    ok

---
+++ src/routes/+page.js
@@ -0,0 +1,9 @@
+export function load({ params }) {
+    console.log(params);
+    return {
+      post: {
+        title: `Title for ${params.slug} goes here`,
+        content: `Content for ${params.slug} goes here`
+      }
+    };
+  }
