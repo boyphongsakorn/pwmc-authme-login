e3c8a1713dedb1b9e86085d1eb5e158cbe0df999
Phongsakorn Wisetthon <boy1556@hotmail.com>
2023-02-24 08:56:53 +0700

    ok

--- src/routes/hooks.server.ts
+++ src/routes/hooks.server.ts
@@ -2,6 +2,7 @@ import type { Handle } from '@sveltejs/kit'
 
 
 export const handle: Handle = async ({ event, resolve }) => {
+    console.log(event);
     const session = event.cookies.get('disco_access_token');
     if (session) {
         console.log('session exists');
