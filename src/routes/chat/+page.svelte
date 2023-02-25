bc9c32c8ba425bc44d3c6c86486d7f1f07bb76c2
Phongsakorn Wisetthon <boy1556@hotmail.com>
2023-02-25 18:08:32 +0700

    ok

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -246,18 +246,6 @@
 					})
 				);
 			});
-			//split message and check if all split message is same
-			allchat.forEach((item) => {
-				let splitmessage = item.message.split(' ');
-				//check if all split message is same
-				let isallsame = splitmessage.every((val, i, arr) => val === arr[0]);
-				if (isallsame) {
-					//if all split message is same, remove it
-					item.message = '';
-				}
-			});
-			//remove blank message
-			allchat = allchat.filter((item) => item.message !== '');
 			allchat.forEach(function (item) {
 				messages = [...messages, item.message];
 				messagesinfo = [...messagesinfo, item.user];
@@ -371,18 +359,6 @@
 						})
 					);
 				});
-				//split message and check if all split message is same
-				allchat.forEach((item) => {
-					let splitmessage = item.message.split(' ');
-					//check if all split message is same
-					let isallsame = splitmessage.every((val, i, arr) => val === arr[0]);
-					if (isallsame) {
-						//if all split message is same, remove it
-						item.message = '';
-					}
-				});
-				//remove blank message
-				allchat = allchat.filter((item) => item.message !== '');
 				if (allchat.length > messages.length) {
 					//order by time
 					allchat.sort(function (a, b) {
