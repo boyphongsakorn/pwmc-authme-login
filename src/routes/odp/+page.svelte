0bbfb56151977744f1fb8e9a384ea195f85fdca3
Phongsakorn Wisetthon <boy1556@hotmail.com>
2023-03-07 08:09:48 +0700

    ok

--- src/routes/odp/+page.svelte
+++ src/routes/odp/+page.svelte
@@ -147,8 +147,11 @@
         async function getnexttimerun() {
             const response = await fetch('https://anywhere.pwisetthon.com/https://cpsql.pwisetthon.com/oneday/allevent');
             const json = await response.json();
+            json.sort(function (a, b) {
+                return b.event_start - a.event_start;
+            });
             //if last event is not waiting
-            if (json[0].status != 'waiting' || json[0].status != 'start') {
+            if (json[0].status != 'waiting' && json[0].status != 'start') {
                 //set next event as first event
                 let today = new Date();
                 //get timeleft from now to 23:30:00
